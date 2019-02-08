5778f2bfb49d26668eb6faca5a084e73dea06974
Kelemen Szimonisz via Macbook Pro <kelemen.szimonisz@macbookpro>
2019-02-07 18:16:07 -0800

    implemented modular IP address designation, access to gyroscope,magnetometer,acceloremeter,etc

--- Arduino/biscuit_euler_wifi/biscuit_euler_wifi.ino
+++ Arduino/biscuit_euler_wifi/biscuit_euler_wifi.ino
@@ -4,10 +4,16 @@
 
 Adafruit_BNO055 bno055 = Adafruit_BNO055();
 WiFiUDP Udp;
+
 char* wifiSSID = "INSERT SSID HERE";
 char* wifiPassword = "INSERT WIFI PASSWORD HERE";
+
+// IP address must be seperated via commas, rather than periods:
+IPAddress packetDestinationIP(01,123,123,123);
+// Change this variable to change the port the biscuit data is sent to.
 unsigned int localUdpPort = 4210;
-char message[256];
+
+char message[1024];
 
 void setup()
 {
@@ -18,7 +24,7 @@ void setup()
   Serial.begin(115200);
   Udp.begin(localUdpPort);
   
-  //start the sensor
+  // start the sensor
   bno055.begin();
   Serial.println();
 
@@ -33,26 +39,40 @@ void setup()
   Serial.println();
 
   // Biscuit LED turns on when connected to WiFi
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
 
-    // Obtain euler angle vector from Orientation Sensor
+    // Obtain Euler Angle vector from Orientation Sensor (values in degrees)
     imu::Vector<3> euler = bno055.getVector(Adafruit_BNO055::VECTOR_EULER);
+    // Magnetometer xyz values in uT (micro Teslas)
+    imu::Vector<3> magnetometer = bno055.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+    // Gyroscope xyz values in rps (radians per second)
+    imu::Vector<3> gyroscope = bno055.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+    // Accelerometer xyz values in m/s^2
+    imu::Vector<3> accelerometer = bno055.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    // Linear Acceleration xyz values in m/s^2
+    imu::Vector<3> linear_accel = bno055.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+    // Gravity xyz values in m/s^2
+    imu::Vector<3> gravity = bno055.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+
+    // Quaternion values (qW,qX,qY,qZ)
+    imu::Quaternion quat = bno055.getQuat();
+
+    //Udp.beginPacket(packetDestinationIP,localUdpPort);
+    Udp.beginPacket(remote,localUdpPort);
     
-    // Print euler angles as serial output
-    //Serial.println("[EULER ANGLES]");
-    //Serial.println(euler.x());
-    //Serial.println(euler.y());
-    //Serial.println(euler.z());
+    sprintf(message,"[Euler Angles]\nx: %f\ny: %f\nz: %f\n",euler.x(),euler.y(),euler.z());
+    sprintf(message+strlen(message),"[Magnetometer]\nx: %f\ny: %f\nz: %f\n",euler.x(),magnetometer.y(),magnetometer.z());
+    sprintf(message+strlen(message),"[Gyroscope]\nx: %f\ny: %f\nz: %f\n",gyroscope.x(),gyroscope.y(),gyroscope.z());
+    sprintf(message+strlen(message),"[Accelerometer]\nx: %f\ny: %f\nz: %f\n",accelerometer.x(),accelerometer.y(),accelerometer.z());
+    sprintf(message+strlen(message),"[Linear Acceleration]\nx: %f\ny: %f\nz: %f\n",linear_accel.x(),linear_accel.y(),linear_accel.z());
+    sprintf(message+strlen(message),"[Gravity]\nx: %f\ny: %f\nz: %f\n",gravity.x(),gravity.y(),gravity.z());
+    sprintf(message+strlen(message),"[Quaternion]\nqW: %f\nqX: %f\nqY: %f\nqZ: %f\n",quat.w(),quat.x(),quat.y(),quat.z());
 
-    // Send packet to port 4210
-    Udp.beginPacket(Udp.remoteIP(),4210);
-
-    sprintf(message,"[BISCUIT Euler Angles]\nx: %f\ny: %f\nz: %f\n",euler.x(),euler.y(),euler.z());
     Udp.write(message);
     Udp.endPacket();
     
